05ac6063a9918518b9e71a34d15a7e252cfd3e62
Arman <2001armanmaulana@gmail.com>
2022-06-09 17:15:54 +0800

    custom date picker

---
+++ lib/widgets/custom_date_picker.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+class CustomDatePicker extends StatefulWidget {
+  const CustomDatePicker(
+      {Key? key,
+      required this.title,
+      required this.hint,
+      required this.controller})
+      : super(key: key);
+
+  final String title, hint;
+  final TextEditingController controller;
+
+  @override
+  State<CustomDatePicker> createState() => _CustomDatePickerState();
+}
+
+class _CustomDatePickerState extends State<CustomDatePicker> {
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 10),
+        Text(widget.title),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: widget.controller,
+                decoration: InputDecoration(
+                  hintText: widget.hint,
+                  hintStyle: TextStyle(fontSize: 13),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                DatePicker.showDatePicker(
+                  context,
+                  showTitleActions: true,
+                  minTime: DateTime(2020, 3, 5),
+                  maxTime: DateTime.now(),
+                  onChanged: (date) {
+                    String tanggal =
+                        DateFormat("EEEE, d MMMM yyyy", "id_ID").format(date);
+                    widget.controller.text = tanggal;
+                    setState(() {});
+                  },
+                  onConfirm: (date) {
+                    String tanggal =
+                        DateFormat("EEEE, d MMMM yyyy", "id_ID").format(date);
+                    widget.controller.text = tanggal;
+                    setState(() {});
+                  },
+                  currentTime: DateTime.now(),
+                  locale: LocaleType.id,
+                );
+              },
+              child: Container(
+                width: 40,
+                height: 40,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/calendar.png"),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
