20657d3b8305aaebe9afa8d5fd5951d85cc55ded
Arman <2001armanmaulana@gmail.com>
2022-06-09 17:15:46 +0800

    custom card sektor

---
+++ lib/widgets/custom_card_sektor.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mandiriapp/constans.dart';
+
+class CustomCardSektor extends StatelessWidget {
+  const CustomCardSektor(
+      {Key? key,
+      required this.title,
+      required this.kunjungan,
+      required this.onTap,
+      this.canDelete = false,
+      this.isSektor = false})
+      : super(key: key);
+
+  final String title;
+  final int kunjungan;
+  final VoidCallback onTap;
+  final bool canDelete, isSektor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        margin: EdgeInsets.only(bottom: 10),
+        child: Row(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: isSektor
+                      ? AssetImage("assets/mountain.png")
+                      : AssetImage("assets/user_circle.png"),
+                ),
+              ),
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text("Total kunjungan : $kunjungan"),
+              ],
+            ),
+            canDelete
+                ? Flexible(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: redMain,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Hapus",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                : SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
