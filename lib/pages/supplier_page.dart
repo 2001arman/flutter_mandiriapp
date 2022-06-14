d8f803534330d00304c6a736ed562a13f7734137
Arman <2001armanmaulana@gmail.com>
2022-06-14 15:05:36 +0800

    supplier page

---
+++ lib/pages/supplier_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SupplierPage extends StatelessWidget {
+  const SupplierPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Supplier Page"),
+    );
+  }
+}
