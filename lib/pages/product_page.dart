fe26ab6d3222dfd8b54e41d8dca7cd49210ee012
Arman <2001armanmaulana@gmail.com>
2022-06-14 15:05:29 +0800

    product page

---
+++ lib/pages/product_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Product Page"),
+    );
+  }
+}
