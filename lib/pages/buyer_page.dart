e0d13d2239af8e1348195968d8898608aa08ab43
Arman <2001armanmaulana@gmail.com>
2022-06-14 15:05:21 +0800

    buyer page

---
+++ lib/pages/buyer_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BuyerPage extends StatelessWidget {
+  const BuyerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Buyer Page"),
+    );
+  }
+}
