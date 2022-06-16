b1098c0d69a8ea1ad23f4f397f98c7596697dc97
Arman <2001armanmaulana@gmail.com>
2022-06-16 17:54:51 +0800

    + function to login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_mandiriapp/cubit/auth_cubit.dart';
 import 'package:flutter_mandiriapp/widgets/custom_button.dart';
 import 'package:flutter_mandiriapp/widgets/custom_text_field.dart';
 
@@ -11,7 +13,7 @@ class LoginPage extends StatelessWidget {
     final TextEditingController controllerPassword = TextEditingController();
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 40),
+        padding: const EdgeInsets.symmetric(horizontal: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -19,7 +21,7 @@ class LoginPage extends StatelessWidget {
               "assets/logo.png",
               width: double.infinity,
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             CustomTextField(
               title: "Username",
               controller: controllerUsername,
@@ -31,12 +33,36 @@ class LoginPage extends StatelessWidget {
               hint: "Masukkan password anda",
               isSecure: true,
             ),
-            SizedBox(height: 40),
-            CustomButton(
-              text: "LOGIN",
-              onTap: () {
-                Navigator.pushNamedAndRemoveUntil(
-                    context, 'main-page', (route) => false);
+            const SizedBox(height: 40),
+            BlocConsumer<AuthCubit, AuthState>(
+              listener: (context, state) {
+                if (state is AuthSuccess) {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, 'main-page', (route) => false);
+                } else if (state is AuthFailed) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      // backgroundColor: kGreenLightColor,
+                      content: Text(state.error),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is AuthLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return CustomButton(
+                  text: "LOGIN",
+                  onTap: () {
+                    context.read<AuthCubit>().signIn(
+                          email: controllerUsername.text,
+                          password: controllerPassword.text,
+                        );
+                  },
+                );
               },
             ),
           ],
