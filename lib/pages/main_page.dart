72f8a392c3210eff76d51bbbd0a4d3decf5f1337
Arman <2001armanmaulana@gmail.com>
2022-06-21 17:59:16 +0800

    add function to each button

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_mandiriapp/constans.dart';
+import 'package:flutter_mandiriapp/cubit/kelapa_cubit.dart';
+import 'package:flutter_mandiriapp/pages/kelapa_page.dart';
 import 'package:flutter_mandiriapp/pages/kunjungan_page.dart';
+import 'package:flutter_mandiriapp/pages/produk_page.dart';
+import 'package:flutter_mandiriapp/pages/supplier_page.dart';
 import 'package:flutter_mandiriapp/widgets/custom_card_sektor.dart';
 
 class MainPage extends StatelessWidget {
@@ -12,7 +17,7 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: blueMain,
         title: Text(
-          "SEKTOR",
+          "Mandiri App",
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w700,
@@ -25,54 +30,65 @@ class MainPage extends StatelessWidget {
         child: ListView(
           children: [
             CustomCardSektor(
-              title: "Pertambangan",
-              kunjungan: 12,
+              title: "Kelapa Sawit",
+              kunjungan: "Total Nasabah: 4",
               isSektor: true,
               onTap: () {
+                context.read<KelapaCubit>().getKelapa();
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: ((context) =>
-                        KunjunganPage(title: "Pertambangan")),
+                    builder: ((context) => KelapaPage()),
                   ),
                 );
               },
             ),
             CustomCardSektor(
               title: "Port",
-              kunjungan: 12,
+              kunjungan: "Total Nasabah: 12",
+              isSektor: true,
+              onTap: () {},
+            ),
+            CustomCardSektor(
+              title: "Pertambangan",
+              kunjungan: "Total Nasabah: 4",
               isSektor: true,
               onTap: () {
+                context.read<KelapaCubit>().getTambang();
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: ((context) => KunjunganPage(title: "Port")),
+                    builder: ((context) => const KunjunganPage(
+                          title: "Pertambangan",
+                        )),
                   ),
                 );
               },
             ),
             CustomCardSektor(
-              title: "Pertanian",
-              kunjungan: 8,
+              title: "Produk",
+              kunjungan: "Total Nasabah: 12",
               isSektor: true,
               onTap: () {
+                context.read<KelapaCubit>().getProduk();
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: ((context) => KunjunganPage(title: "Pertanian")),
+                    builder: ((context) => const ProdukPage()),
                   ),
                 );
               },
             ),
             CustomCardSektor(
-              title: "Pembangunan",
-              kunjungan: 23,
+              title: "Supplier & Buyer",
+              kunjungan: "Total Nasabah: 12",
               isSektor: true,
               onTap: () {
+                context.read<KelapaCubit>().getSupplier();
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: ((context) => KunjunganPage(title: "Pembangunan")),
+                    builder: ((context) => const SupplierPage()),
                   ),
                 );
               },
