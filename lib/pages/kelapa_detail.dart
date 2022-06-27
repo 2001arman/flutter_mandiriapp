b9f6516bf3fc7c26dd6b63fd63db5da64d4095c2
Arman <2001armanmaulana@gmail.com>
2022-06-27 12:43:38 +0800

    kelapa detail

--- lib/pages/kelapa_detail.dart
+++ lib/pages/kelapa_detail.dart
@@ -2,11 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mandiriapp/constans.dart';
 import 'package:flutter_mandiriapp/models/kelapa_model.dart';
 import 'package:flutter_mandiriapp/widgets/custom_text_detail.dart';
+import 'package:flutter_mandiriapp/widgets/custom_text_field.dart';
 
-class KelapaDetail extends StatelessWidget {
+class KelapaDetail extends StatefulWidget {
   const KelapaDetail({Key? key, required this.kelapaContent}) : super(key: key);
 
   final Content kelapaContent;
+
+  @override
+  State<KelapaDetail> createState() => _KelapaDetailState();
+}
+
+class _KelapaDetailState extends State<KelapaDetail> {
+  TextEditingController controllerKota = TextEditingController();
+  TextEditingController controllerCIF = TextEditingController();
+  TextEditingController controllerKoperasi = TextEditingController();
+  TextEditingController controllerKebun = TextEditingController();
+  TextEditingController controllerLahan = TextEditingController();
+  TextEditingController controllerAnggota = TextEditingController();
+  TextEditingController controllerKodeCabang = TextEditingController();
+  TextEditingController controllerNamaCabang = TextEditingController();
+  TextEditingController controllerArea = TextEditingController();
+  String valueBMRI = 'bmri';
+  String valueDebiture = 'debitur';
+
+  @override
+  void initState() {
+    super.initState();
+    controllerKota.text = widget.kelapaContent.kabupatenKota!;
+    controllerCIF.text = widget.kelapaContent.cif!;
+    controllerKoperasi.text = widget.kelapaContent.namaKoperasi!;
+    controllerKebun.text = widget.kelapaContent.kebunInti!;
+    controllerLahan.text = widget.kelapaContent.luasLahan!;
+    controllerAnggota.text = widget.kelapaContent.jumlahAnggota!;
+    controllerKodeCabang.text = widget.kelapaContent.idCabang!;
+    controllerNamaCabang.text = widget.kelapaContent.namaKoperasi!;
+    controllerArea.text = widget.kelapaContent.area!;
+    valueBMRI = widget.kelapaContent.checkBmri!;
+    valueDebiture = widget.kelapaContent.checkDebitur!;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,42 +56,119 @@ class KelapaDetail extends StatelessWidget {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: blueMain,
+        child: Text(
+          "+",
+          style: TextStyle(
+            fontSize: 30,
+            color: yellowMain,
+          ),
+        ),
+        onPressed: () {},
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: ListView(
-          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CustomTextDetail(
-                title: "Kabupaten/Kota",
-                content: kelapaContent.kabupatenKota.toString()),
-            CustomTextDetail(
-                title: "CIF", content: kelapaContent.cif.toString()),
-            CustomTextDetail(
-                title: "Nama Koperasi",
-                content: kelapaContent.namaKoperasi.toString()),
-            CustomTextDetail(
-                title: "Kebun Inti",
-                content: kelapaContent.kebunInti.toString()),
-            CustomTextDetail(
-                title: "Luas Lahan",
-                content: kelapaContent.luasLahan.toString()),
-            CustomTextDetail(
-                title: "Jumlah Anggota",
-                content: kelapaContent.jumlahAnggota.toString()),
-            CustomTextDetail(
-                title: "Kode Cabang",
-                content: kelapaContent.idCabang.toString()),
-            CustomTextDetail(
-                title: "Nama Cabang",
-                content: kelapaContent.namaKoperasi.toString()),
-            CustomTextDetail(
-                title: "Check BMRI",
-                content: kelapaContent.checkBmri.toString()),
-            CustomTextDetail(
-                title: "Check Debitur",
-                content: kelapaContent.checkDebitur.toString()),
-            CustomTextDetail(
-                title: "Area", content: kelapaContent.area.toString()),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextField(
+                  title: "Kabuptaen/Kota",
+                  hint: "",
+                  controller: controllerKota,
+                ),
+                CustomTextField(
+                  title: "CIF",
+                  hint: "",
+                  controller: controllerCIF,
+                ),
+                CustomTextField(
+                  title: "Nama Koperasi",
+                  hint: "",
+                  controller: controllerKoperasi,
+                ),
+                CustomTextField(
+                  title: "Kebun Inti",
+                  hint: "",
+                  controller: controllerKebun,
+                ),
+                CustomTextField(
+                  title: "Luas Lahan(Ha)",
+                  hint: "",
+                  controller: controllerLahan,
+                ),
+                CustomTextField(
+                  title: "Jumlah Anggota",
+                  hint: "",
+                  controller: controllerAnggota,
+                ),
+                CustomTextField(
+                  title: "Kode Cabang",
+                  hint: "",
+                  controller: controllerKodeCabang,
+                ),
+                CustomTextField(
+                  title: "Nama Cabang",
+                  hint: "",
+                  controller: controllerNamaCabang,
+                ),
+                const SizedBox(height: 5),
+                const Text("Check BMRI"),
+                DropdownButton<String>(
+                  value: valueBMRI,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      valueBMRI = newValue!;
+                    });
+                  },
+                  items: <String>['bmri', 'nonbmri']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 5),
+                const Text("Check Debitur"),
+                DropdownButton<String>(
+                  value: valueDebiture,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      valueDebiture = newValue!;
+                    });
+                  },
+                  items: <String>['debitur', 'nondebitur']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                CustomTextField(
+                  title: "Area",
+                  hint: "",
+                  controller: controllerArea,
+                ),
+              ],
+            ),
           ],
         ),
       ),
